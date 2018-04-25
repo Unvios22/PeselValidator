40c7a8d92ddae7e365d92a35a76107415974e4f1
Unvios22 <unvios22@gmail.com>
2018-04-25 21:54:00 +0200

    Adding basic classes

---
+++ PeselValidator/PeselValidator/Pesel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeselValidator
+{
+    class Pesel
+    {
+        private String pesel;
+        private bool valid;
+        private DateTime birthDate;
+        private char sex;
+
+        Pesel() {
+
+        }
+
+
+
+
+    }
+
+    
+
+
+}
---
+++ PeselValidator/PeselValidator/ValidatePesel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeselValidator
+{
+    class ValidatePesel
+    {
+        private int[] peselArray;
+        private bool validLength;
+        private bool validFormat;
+        private bool validControlNumber;
+        private bool validDate;
+    }
+}
